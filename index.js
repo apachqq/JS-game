de3ba28cd26f74d5ca0dbfa34bb96e2a74ea47b2
apachqq <work.tsyganov@mail.ru>
2022-03-29 01:27:28 +0300

    'event_and_function_startGame'

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let $start = document.querySelector('#start')
 let $game = document.querySelector('#game')
+
+$start.addEventListener('click', startGame)
+
+function startGame() {
+    $game.style.backgroundColor = '#fff'
+    $start.classList.add('hide')
+}
