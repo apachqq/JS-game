08f589e66e0d85905f813a1122b037bf3e759d32
apachqq <work.tsyganov@mail.ru>
2022-03-31 04:36:53 +0300

    'show_and_hide_in_fucntion_setGameTime_and_endGame'

--- index.js
+++ index.js
@@ -51,16 +51,19 @@ function setGameScore() {
 function setGameTime() {
     let time = 5
     $time.textContent = time.toFixed(1)
+    show($timeHeader)
+    hide($resultHeader)
 }
 
 function endGame() {
     isGameStarted = false
     setGameScore()
-    $start.classList.remove('hide')
+    $gameTime.removeAttribute('disabled')
+    show($start)
     $game.innerHTML = ''
     $game.style.backgroundColor = '#ccc'
-    $timeHeader.classList.add('hide')
-    $resultHeader.classList.remove('hide')
+    hide($timeHeader)
+    show($resultHeader)
 }
 
 function handleBoxClick(event) {
